930fa8ba39f7c94984196f166505adfdc29f15ec
hct-ux <henry.thomas@gbgplc.com>
2022-06-23 09:42:49 +0100

    added integration details

--- src/routes/component/index.js
+++ src/routes/component/index.js
@@ -1,5 +1,6 @@
 import { h } from "preact";
 import { Suspense } from "preact/compat";
+import { useState } from "preact/hooks";
 import { usePrerenderData } from "@preact/prerender-data-provider";
 import Markdown from "markdown-to-jsx";
 import Example from "../../components/example";
@@ -9,6 +10,7 @@ import style from "./style";
 
 const component = (props) => {
   const [data, isLoading] = usePrerenderData(props);
+
   return (
     <article class={style.componentcontainer}>
       {getComponentBody(data, isLoading)}
@@ -32,30 +34,58 @@ function CodeBlock(props) {
   );
 }
 
-function InlineImage({ alt, title, src }) {
+function usageDetails(details) {
   return (
-    <div class={style.inlineImageContainer}>
-      <img class={style.inlineImage} src={src} alt={alt} />
-      {title && <span class={style.inlineImageTitle}>{title}</span>}
-    </div>
+    <Markdown
+      options={{
+        overrides: {
+          img: {
+            component: InlineImage,
+          },
+          code: {
+            component: CodeBlock,
+          },
+        },
+      }}
+    >
+      {details.integration}
+    </Markdown>
   );
 }
 
-function getComponentBody(data, isLoading) {
-  if (isLoading) {
-    return <div class="spinner"></div>;
-  }
-
-  if (data && data.data) {
-    const { details, content } = data.data;
-    return (
-      <div>
-        <h1 class={style.blogtitle}>{details.title}</h1>
-        <div class={style.badges}>
-          <StatusBadge component={details} />
-          <WCAGStatusBadge component={details} />
-        </div>
-        <h2 class="m-m-t-8">Description</h2>
+function generalDetails(details, content) {
+  return (
+    <div>
+      <h2>Description</h2>
+      <Markdown
+        options={{
+          overrides: {
+            img: {
+              component: InlineImage,
+            },
+            code: {
+              component: CodeBlock,
+            },
+          },
+        }}
+      >
+        {details.description}
+      </Markdown>
+      <br />
+      <br />
+      {details.cover && (
+        <div
+          class={style.blogcover}
+          style={`background-image:url(${details.cover})`}
+        />
+      )}
+      <div class={style.examples}>
+        <h2>Examples</h2>
+        {details.examples.map((example) => {
+          return <Example example={example} />;
+        })}
+      </div>
+      <div class={style.blogbody}>
         <Markdown
           options={{
             overrides: {
@@ -68,58 +98,98 @@ function getComponentBody(data, isLoading) {
             },
           }}
         >
-          {details.description}
+          {content}
         </Markdown>
-        <br />
-        <br />
-        {details.cover && (
-          <div
-            class={style.blogcover}
-            style={`background-image:url(${details.cover})`}
-          />
-        )}
-        <div class={style.examples}>
-          <h2>Examples</h2>
-          {details.examples.map((example) => {
-            return <Example example={example} />;
-          })}
-        </div>
-        <div class={style.blogbody}>
-          <Markdown
-            options={{
-              overrides: {
-                img: {
-                  component: InlineImage,
-                },
-                code: {
-                  component: CodeBlock,
-                },
-              },
-            }}
-          >
-            {content}
-          </Markdown>
-          <div class={style.doanddont}>
-            <div class={style.dos}>
-              <h3>Do...</h3>
-              <ul>
-                {details.dos.map((dont) => (
-                  <li>{dont}</li>
-                ))}
-              </ul>
-            </div>
-            <div class={style.donts}>
-              <h3>Don't...</h3>
-              <ul>
-                {details.donts.map((dont) => (
-                  <li>{dont}</li>
-                ))}
-              </ul>
-            </div>
+        <div class={style.doanddont}>
+          <div class={style.dos}>
+            <h3>Do...</h3>
+            <ul>
+              {details.dos.map((dont) => (
+                <li>{dont}</li>
+              ))}
+            </ul>
+          </div>
+          <div class={style.donts}>
+            <h3>Don't...</h3>
+            <ul>
+              {details.donts.map((dont) => (
+                <li>{dont}</li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
+}
+
+function InlineImage({ alt, title, src }) {
+  return (
+    <div class={style.inlineImageContainer}>
+      <img class={style.inlineImage} src={src} alt={alt} />
+      {title && <span class={style.inlineImageTitle}>{title}</span>}
+    </div>
+  );
+}
+
+function header(details) {
+  return (
+    <>
+      <h1 class={style.blogtitle}>{details.title}</h1>
+      <div class={style.badges}>
+        <StatusBadge component={details} />
+        <WCAGStatusBadge component={details} />
+      </div>
+    </>
+  );
+}
+
+function getComponentBody(data, isLoading) {
+  const [tab, setTab] = useState("details");
+  if (isLoading) {
+    return <div class="spinner"></div>;
+  }
+
+  if (data && data.data) {
+    const { details, content } = data.data;
+    if (details.integration) {
+      return (
+        <>
+          {header(details)}
+          <ul class="tab-bar w-100">
+            <li
+              onClick={() => {
+                setTab("details");
+              }}
+              class={`tab ${tab === "details" ? "tab--active" : ""}`}
+              tabindex="0"
+            >
+              Details
+            </li>
+            <li
+              onClick={() => {
+                setTab("usage");
+              }}
+              class={`tab ${tab === "usage" ? "tab--active" : ""}`}
+              tabindex="0"
+            >
+              Usage Information
+            </li>
+          </ul>
+          <div class={style.content}>
+            {tab === "details" && generalDetails(details, content)}
+            {tab === "usage" && usageDetails(details, content)}
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          {header(details)}
+          <div class={style.content}>{generalDetails(details, content)}</div>
+        </>
+      );
+    }
   }
 }
 
